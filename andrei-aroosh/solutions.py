8bbebda62482f939c5960f6494a97e072a94bfb2
Wesley Nguyen <nguyenw@seattleu.edu>
2021-01-06 18:33:16 -0800

    init first problem

--- andrei-aroosh/solutions.py
+++ andrei-aroosh/solutions.py
@@ -1,3 +1,24 @@
 '''
 This is a colab workspace for Andrei and Aroosh
 '''
+
+
+'''
+Problem 1
+
+Two Sum: Given an array of integers and a target value, return the indicies of the integers in the array that add up to the target value. Return these values in a list.
+
+INPUT: [10, 33, 55, 2, 68, 100], 88
+
+OUTPUT: [1, 2]
+
+'''
+
+def twosum(arr:list, trg:int) -> list:
+    res = []
+
+
+    return res
+
+
+assert twosum([10, 33, 55, 2, 68, 100], 88) == [1, 2]
