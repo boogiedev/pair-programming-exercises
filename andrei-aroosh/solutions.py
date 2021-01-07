0f9a2428b1d20d07838b582ba5923d6620ba352a
Wesley Nguyen <nguyenw@seattleu.edu>
2021-01-06 18:47:03 -0800

    init problem 2

--- andrei-aroosh/solutions.py
+++ andrei-aroosh/solutions.py
@@ -1,3 +1,4 @@
+import random
 '''
 This is a colab workspace for Andrei and Aroosh
 '''
@@ -21,11 +22,25 @@ def twosum(arr:list, trg:int) -> list:
             if arr[x] == needed_num:
                 return [i,x]
 
-
-
-
-
-
-
-
 assert twosum([10, 33, 55, 2, 68, 100], 88) == [1, 2]
+
+
+
+'''
+Problem 2
+
+Clean the noise: Given a string of what is SUPPOSED to be a normal sentence, return the CLEANED version of the sentence. This means that there will be random punctuation inserted in the sentence.
+
+INPUT: "I& #lo!ve% to %c!o%d&e %a@n^d ^c!ol#ab% ^o!n g$i&t@hub# w%ith $m$y& fo@lks!"
+
+OUTPUT: "I love to code and colab on github with my folks"
+
+'''
+
+def clean_string(noisy_str:str) -> str:
+    res = ''
+
+
+    return res
+
+assert clean_string("""I& #lo!ve% to %c!o%d&e %a@n^d ^c!ol#ab% ^o!n g$i&t@hub# w%ith $m$y& fo@lks!""") == "I love to code and colab on github with my folks"
