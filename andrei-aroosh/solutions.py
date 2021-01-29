59a33b13984225f2de5fc2c9468d0a22e7c2b92c
Wesley Nguyen <nguyenw@seattleu.edu>
2021-01-28 18:58:48 -0800

    created problem 8

--- andrei-aroosh/solutions.py
+++ andrei-aroosh/solutions.py
@@ -285,7 +285,46 @@ assert kidsWithCandies(candies=[4,2,1,1,2], extraCandies=1) == [True,False,False
 
 
 
+"""
+Problem 8:
+
+Design a Parking System
+
+Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+Implement the ParkingSystem class:
+
+ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
 
 
+Example 1:
+
+Input:
+["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+[[1, 1, 0], [1], [2], [3], [1]]
+
+Output:
+[null, true, true, false, false]
+
+
+"""
+
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        pass
+
+    def addCar(self, carType: int) -> bool:
+        pass
+
+
+bellevue_square = ParkingSystem(2, 1, 1)
+# This is possible
+bellevue_square.addCar(1)
+# This is possible
+bellevue_square.addCar(2)
+# This is NOT possible
+bellevue_square.addCar(2)
 
 #
